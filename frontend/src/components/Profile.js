795909476fca809937fe03baa12545fa76a590d1
Wantei Kupar Warjri <wantei202006@gmail.com>
2022-08-02 23:49:17 +0530

    Set max width to 100%

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -124,10 +124,10 @@ class Profile extends React.Component {
       this.props.profile.username === this.props.currentUser.username;
 
     return (
-      <div className="profile-page row">
+      <div className="profile-page mw-100">
         <div className="container">
           <div className="row p-4 text-center">
-            <div className="col-xs-12 col-md-8 offset-md-2">
+            <div className="user-info col-xs-12 col-md-8 offset-md-2">
               <img
                 src={profile.image}
                 className="user-img"
