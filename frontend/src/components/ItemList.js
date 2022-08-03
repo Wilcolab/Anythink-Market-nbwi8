317d719ed3ec12e8ac7ce20c679c13fd5b4406d9
Wantei Kupar Warjri <wantei202006@gmail.com>
2022-08-03 16:58:16 +0530

    Added sad message

--- frontend/src/components/ItemList.js
+++ frontend/src/components/ItemList.js
@@ -1,20 +1,46 @@
 import ItemPreview from "./ItemPreview";
 import ListPagination from "./ListPagination";
 import React from "react";
+import { ReactComponent as SadImg } from "../imgs/face-sad-tear.svg";
 
 const ItemList = (props) => {
   if (!props.items) {
     return <div className="py-4">Loading...</div>;
   }
 
-  if (props.items.length === 0) {
-    return <div className="py-4 no-items">No items are here... yet.</div>;
+  let validSearch =
+    props.items.length === 0 &&
+    (!props.searchText || props.searchText.length < 3);
+
+  if (validSearch) {
+    return (
+      <div className="py-4 no-items">
+        We couldn't find what you're looking for!
+      </div>
+    );
   }
 
   let filteredItems = props.items;
   if (props.searchText && props.searchText.length >= 3) {
     filteredItems = filteredItems.filter((item) =>
       item.title.toLowerCase().includes(props.searchText.toLowerCase().trim())
+    );
+  }
+
+  if (filteredItems.length === 0) {
+    return (
+      <div
+        id="empty"
+        className="w-50 mx-auto my-4 py-4 text-center"
+        style={{ backgroundColor: "hsla(100, 100%, 100%, .1)" }}
+      >
+        <p>
+          <SadImg width="50" fill="currentColor" title="Sad face that found" />
+        </p>
+        <p className="mb-0 text-wrap">
+          No items found for <strong>"{props.searchText}"</strong>.
+        </p>
+      </div>
     );
   }
 
