e3a92d7f1f03f52fa96110db4a4d6d9b9d3b2e91
Wantei Kupar Warjri <wantei202006@gmail.com>
2022-08-03 17:13:36 +0530

    Hide search bar

--- frontend/src/components/Home/SearchBar.js
+++ frontend/src/components/Home/SearchBar.js
@@ -3,6 +3,14 @@ import { connect } from "react-redux";
 import { APPLY_SEARCH_FILTER } from "../../constants/actionTypes";
 
 const SearchBar = (props) => {
+  // Hide the search bar by default
+  const [isSearchHidden, setIsSearchHidden] = React.useState(true);
+
+  const handleOnClick = (event) => {
+    event.preventDefault();
+    setIsSearchHidden(!isSearchHidden);
+  };
+
   const onChangeHandler = (event) => {
     event.preventDefault();
     const { value } = event.target;
@@ -16,26 +24,35 @@ const SearchBar = (props) => {
   return (
     <div className="container p-4 text-center">
       <div className="search-bar row">
-        <div className="col text-right" id="get-part">
+        <div
+          className="col text-right"
+          id="get-part"
+          style={{ cursor: "pointer" }}
+          onClick={handleOnClick}
+        >
           A place to get
         </div>
 
-        <div className="input-group rounded col">
-          <input
-            id="search-box"
-            type="search"
-            className="form-control rounded"
-            placeholder="What is it that you truly desire?"
-            aria-label="Search"
-            aria-describedby="search-addon"
-            onChange={onChangeHandler}
-          />
-          <span className="input-group-text border-0" id="search-addon">
-            <i className="bi bi-search"></i>
-          </span>
-        </div>
+        {isSearchHidden ? (
+          ""
+        ) : (
+          <div className="input-group rounded col">
+            <input
+              id="search-box"
+              type="search"
+              className="form-control rounded"
+              placeholder="What is it that you truly desire?"
+              aria-label="Search"
+              aria-describedby="search-addon"
+              onChange={onChangeHandler}
+            />
+            <span className="input-group-text border-0" id="search-addon">
+              <i className="bi bi-search"></i>
+            </span>
+          </div>
+        )}
 
-        <div className="col text-left" id="get-part">
+        <div className="col text-left">
           the cool stuff.
         </div>
       </div>
