eae5a94706abb056a8e41494154aa92ccac4ef5e
Wantei Kupar Warjri <wantei202006@gmail.com>
2022-10-15 16:42:05 +0530

    Prettier

--- frontend/src/components/Item/CommentInput.js
+++ frontend/src/components/Item/CommentInput.js
@@ -9,41 +9,41 @@ const mapDispatchToProps = (dispatch) => ({
 });
 
 const CommentInput = (props) => {
-    const [body, setBody] = useState("");
+  const [body, setBody] = useState("");
 
-    this.createComment = async (ev) => {
-      ev.preventDefault();
-      agent.Comments.create(this.props.slug, {
-        body: this.state.body,
-      }).then((payload) => {
-        this.props.onSubmit(payload);
-      });
-      setBody("");
-    };
+  this.createComment = async (ev) => {
+    ev.preventDefault();
+    agent.Comments.create(this.props.slug, {
+      body: this.state.body,
+    }).then((payload) => {
+      this.props.onSubmit(payload);
+    });
+    setBody("");
+  };
 
-    return (
-      <form className="card comment-form m-2" onSubmit={this.createComment}>
-        <div className="card-block">
-          <textarea
-            className="form-control"
-            placeholder="Write a comment..."
-            value={body}
-            onChange={setBody}
-            rows="3"
-          ></textarea>
-        </div>
-        <div className="card-footer">
-          <img
-            src={this.props.currentUser.image}
-            className="user-pic mr-2"
-            alt={this.props.currentUser.username}
-          />
-          <button className="btn btn-sm btn-primary" type="submit">
-            Post Comment
-          </button>
-        </div>
-      </form>
-    );
-}
+  return (
+    <form className="card comment-form m-2" onSubmit={this.createComment}>
+      <div className="card-block">
+        <textarea
+          className="form-control"
+          placeholder="Write a comment..."
+          value={body}
+          onChange={setBody}
+          rows="3"
+        ></textarea>
+      </div>
+      <div className="card-footer">
+        <img
+          src={this.props.currentUser.image}
+          className="user-pic mr-2"
+          alt={this.props.currentUser.username}
+        />
+        <button className="btn btn-sm btn-primary" type="submit">
+          Post Comment
+        </button>
+      </div>
+    </form>
+  );
+};
 
 export default connect(() => ({}), mapDispatchToProps)(CommentInput);
